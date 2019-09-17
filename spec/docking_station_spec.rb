85a0dbd945cde48c017f8774dca1c984d83a2798
JDtheGeek <john.dennehy@gmail.com>
2019-09-17 17:24:35 +0100

    formatting

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
     it { is_expected.to respond_to(:release_bike) }
-    it { is_expected.to respond_to(:dock).with(1).argument }
     it { is_expected.to respond_to(:bike) }
+    it { is_expected.to respond_to(:dock).with(1).argument }
 
     it 'releases a working bike' do
          expect(subject.release_bike).to be_working
@@ -14,4 +14,3 @@ describe DockingStation do
         expect(subject.dock(bike)).to eq bike
     end
 end
-

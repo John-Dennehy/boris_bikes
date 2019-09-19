030e6fc1f8fe7ae79be59cdae68ba930a3f276ce
John Dennehy <john.dennehy@gmail.com>
2019-09-19 16:27:58 +0100

    Add 20 bike limit to docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,21 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bikes
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    raise "No bike available" if @bike == nil
-    return @bike 
+    raise "No bike available" if @bikes.empty?
+
+    @bikes.pop
 
   end
 
   def dock(bike)
-    raise "Dock in use" unless @bike == nil
-    @bike = bike
+    raise "Dock is full" if @bikes.size >= 20
+    
+    @bikes << bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require 'docking_station'
 
 describe DockingStation do
     it { is_expected.to respond_to(:release_bike) }
-    it { is_expected.to respond_to(:bike) }
+    it { is_expected.to respond_to(:bikes) }
     it { is_expected.to respond_to(:dock).with(1).argument }
 
     # it 'releases a working bike' do
@@ -10,23 +10,27 @@ describe DockingStation do
     # end
 
     describe '#dock' do
+        
         it 'docks something' do
-            bike = Bike.new
-            expect(subject.dock(bike)).to eq bike
+            my_bike = Bike.new
+            station = DockingStation.new
+            station.dock(my_bike)
+            expect(station.bikes[-1]).to eq my_bike
         end
 
-        it ' raises error if dock already in use' do
-            bike_1 = Bike.new
-            bike_2 = Bike.new
-
-            subject.dock(bike_1)
-
-            expect{subject.dock(bike_2)}.to raise_error("Dock in use")
+        it ' raises error if dock is full' do
+            # setup
+            station = DockingStation.new
+            20.times{station.dock(Bike.new)}
+            my_bike = Bike.new
+            
+           expect{station.dock(my_bike)}.to raise_error("Dock is full")
         end
     end
 
     describe '#release_bike' do
         it 'release a bike' do
+
             # setup
             my_bike = Bike.new
             subject.dock(my_bike)
@@ -38,7 +42,7 @@ describe DockingStation do
             expect(released_bike).to eq my_bike
         end
 
-        it ' raise error if no bike docked' do
+        it 'raise error if no bike docked' do
             expect{subject.release_bike}.to raise_error("No bike available")
         end
     end
